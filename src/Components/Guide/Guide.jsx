49e9a314942eeda64c9c34727096568af6a89bdf
HJ03 <102145684+HJ03@users.noreply.github.com>
2024-03-28 14:15:41 +0530

    mobile guide section added

--- src/Components/Guide/Guide.jsx
+++ src/Components/Guide/Guide.jsx
@@ -10,26 +10,29 @@ function Guide() {
         <div className="container">
           <h1 className='section-heading' style={{ color: "#D4F604" }}>A step guide to being a digital landlord</h1>
           <p className='guide-container-mobile-para'>Powerful, self-serve product and growth analytics to help you convert, engage, and retain more users. Trusted by over 4,000 startups.</p>
-          <div className='blob-mobile-img'>
-            <div className='blob-container'><img src={blobData} alt="" /></div>
-            <div className='mobile-image'><img src={mobilePicture} alt="" /></div>
-          </div>
-          <div className="row">
-            {GuideData.map((item, index) => (
-              <div key={index} className="col-lg-6">
-                <div className="box">
-                  <div className='box-top-header'>
-                    <img src={item.img} alt={item.title} />
-                    <h3 className="box-title">{item.title}</h3>
-                  </div>
-                  <hr />
-                  <p className="paragraph">{item.description}</p>
-                </div>
-              </div>
-            ))}
-          </div>
-
         </div>
+        <div className='blob-mobile-img'>
+          <div className='blob-container'><img src={blobData} alt="" /></div>
+          <div className='mobile-image'><img src={mobilePicture} alt="" /></div>
+        </div>
+        <div className="container">
+        <div className="row">
+          {GuideData.map((item, index) => (
+            <div key={index} className="col-lg-6">
+              <div className="box">
+                <div className='box-top-header'>
+                  <img src={item.img} alt={item.title} />
+                  <h3 className="box-title">{item.title}</h3>
+                </div>
+                <hr />
+                <p className="paragraph">{item.description}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+        </div>
+
+
 
       </section>
     </>
