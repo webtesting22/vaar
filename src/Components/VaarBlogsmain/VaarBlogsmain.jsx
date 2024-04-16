0efbc1d23bb31c17943b354998f2a8f33580a7ab
Sachin-Prepseed <148150265+webtesting22@users.noreply.github.com>
2024-04-16 11:37:34 +0530

    shadow added and hover effects on blogs cards blogs page

--- src/Components/VaarBlogsmain/VaarBlogsmain.jsx
+++ src/Components/VaarBlogsmain/VaarBlogsmain.jsx
@@ -95,7 +95,7 @@ function VaarBlogs() {
                                             <Link to="/VaarBlogCommon">
                                                 <div className='blog-title-container' style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100%", width: "100%" }}>
                                                     <div>
-                                                        <h1><a href="./VaarBlogCommon">{item.blogTitle}</a></h1>
+                                                        <h1 className='main-blog-heading'><a href="./VaarBlogCommon">{item.blogTitle}</a></h1>
                                                         <p>{item.blogContent}</p>
                                                         <div style={{ display: "flex", alignItems: "center" }}>
                                                             <span style={{ margin: "0px 10px", display: "flex", alignItems: "center" }}><i class="bi bi-person-circle fs-5"></i> </span> <p style={{ margin: "0px", fontSize: "14px" }}> {item.authorName}</p>
@@ -126,7 +126,7 @@ function VaarBlogs() {
                                             <Link to="/VaarBlogCommon">
                                                 <div className='blog-title-container' style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100%", width: "100%" }}>
                                                     <div>
-                                                        <h1>{item.blogTitle}</h1>
+                                                        <h1 className='main-blog-heading'>{item.blogTitle}</h1>
                                                         <p>{item.blogContent}</p>
                                                         <div style={{ display: "flex", alignItems: "center" }}>
                                                             <span style={{ margin: "0px 10px", display: "flex", alignItems: "center" }}><i class="bi bi-person-circle fs-5"></i> </span> <p style={{ margin: "0px", fontSize: "14px" }}> {item.authorName}</p>
