8ef3694117032ce711525d531a237dc1d9ea8176
Sachin-Prepseed <148150265+webtesting22@users.noreply.github.com>
2024-04-19 12:34:22 +0530

    Update Footer.jsx

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import "./Footer.css"
-import footerlogo from "../pictures/footerlogo.svg"
+import footerlogo from "../pictures/newlogoUpdate.png"
 function Footer() {
     return (
         <>
