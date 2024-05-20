be95819ad5b7244d54752346c86bf8bb3e84372d
webtesting22 <148150265+webtesting22@users.noreply.github.com>
2024-05-20 11:05:44 +0530

    common blog page layout change

--- src/Components/VaarBlogsmain/VaarBlogs/VaarBlogCommon.jsx
+++ src/Components/VaarBlogsmain/VaarBlogs/VaarBlogCommon.jsx
@@ -7,13 +7,14 @@ import SignupSection from "../../SignUp/SignupSection";
 import { useLocation } from "react-router-dom";
 import BlogCarousal from "./BlogCarousal";
 import { useParams } from "react-router-dom";
+import { Row, Col } from "antd";
 const VaarBlogCommon = (props) => {
     const location = useLocation();
     console.log("Location:", location); // Check the location object
 
     // Extract data from search string
     const params = new URLSearchParams(location.search);
-    const image=params.get("image");
+    const image = params.get("image");
     const title = params.get('title');
     const Description = params.get('Description');
     const date = params.get('date');
@@ -44,12 +45,92 @@ const VaarBlogCommon = (props) => {
                         <p>{content}</p>
                         <p>{date}</p>
                     </div> */}
-                    <div className="blog-top-part" >
+                    {/* <div className="blog-top-part" >
                         <h6 className="publishDate" style={{ color: "white" }}>Published 20 Jan 2024</h6>
                         <h1 style={{ color: "#d4f604", fontSize: "50px", fontWeight: "300" }}>{title}</h1>
                         <div className="blog-section-spacer"></div>
-                        <img src={image} alt="" className="blog-common-image"/>
-                    </div>
+                        <img src={image} alt="" className="blog-common-image" />
+                    </div> */}
+                    <div className="blog-section-spacer"></div>
+                    <div className="blog-section-spacer"></div>
+                    <div className="blog-section-spacer"></div>
+                    <Row
+
+                        // className="blog-container-row"
+                        // style={{ borderBottom: "1px solid rgb(196, 196, 196)" }}
+                    >
+                    <Col lg={12} md={12}>
+
+                        <div
+                            className="blog-title-container"
+                            style={{
+                                display: "flex",
+                                justifyContent: "center",
+                                alignItems: "center",
+                                height: "100%",
+                                width: "100%",
+                            }}
+                        >
+                            <div>
+
+                                <h1 className="main-blog-heading" style={{ color: "#D4F604" }}>
+                                    {/* <a
+                                  // href="./VaarBlogCommon"
+                                  style={{ fontWeight: "400", color: "#D4F604" }}
+                                > */}
+                                    {title}
+                                    {/* </a> */}
+                                </h1>
+
+                                {/* <p className="text-white">{item.blogContent}</p> */}
+                                <div
+                                    style={{ display: "flex", alignItems: "center" }}
+                                >
+                                    <span
+                                        style={{
+                                            marginRight: "10px",
+                                            display: "flex",
+                                            alignItems: "center",
+                                            color: "white",
+                                        }}
+                                    >
+                                        <i
+                                            class="bx bxs-calendar"
+                                            style={{ fontSize: "20px" }}
+                                        ></i>
+                                    </span>
+                                    <p
+                                        style={{
+                                            margin: "0px",
+                                            color: "white",
+                                            fontSize: "14px",
+                                        }}
+                                    >
+                                        Published 20 Jan 2024
+                                        {/* {item.Date} */}
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+
+                    </Col>
+                    <Col
+                        lg={12}
+                        md={12}
+                        style={{
+                            display: "flex",
+                            alignItems: "center",
+                            justifyContent: "center",
+                        }}
+                    >
+
+                        <div className="img-style-container">
+                            <img src={image} alt="" style={{ borderRadius: "5px" }} />
+                            {/* {console.log("imagesfsfsfsfs", blog.image)} */}
+                        </div>
+
+                    </Col>
+                    </Row>
                     <div className="blog-content-container text-white">
                         <div dangerouslySetInnerHTML={{ __html: Description }} />
                         {/* <div dangerouslySetInnerHTML={{ __html: Comment }} /> */}
